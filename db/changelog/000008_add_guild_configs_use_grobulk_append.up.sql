d733ddb167c9233ab7d9fc5bc9f131235851a344
verzac <benjamin.tanone@gmail.com>
2025-10-08 11:36:37 +0700

    more hlepful comments

--- db/changelog/000008_add_guild_configs_use_grobulk_append.up.sql
+++ db/changelog/000008_add_guild_configs_use_grobulk_append.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `guild_configs` ADD COLUMN
   `use_grobulk_append` boolean DEFAULT false;
 
--- set all existing guild configs to false (we assume that they're using the old grobulk behaviour, so make this opt-in for existing users)
+-- set all existing guild configs to true - we assume that they're using the old grobulk behaviour, so make this opt-in through /config for existing users
 UPDATE `guild_configs` SET `use_grobulk_append` = true;
