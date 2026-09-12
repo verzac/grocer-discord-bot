bf632d6101f63d0472acb94c7d2ea4b60cb4489c
verzac <benjamin.tanone@gmail.com>
2026-09-12 23:21:59 +1000

    chore(e2e): add e2e_api to make full_e2e

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -9,6 +9,12 @@ go build -o main
 set +e
 make e2e
 exit_code=$?
+if [ "$exit_code" -eq 0 ]; then
+  sqlite3 db/gorm.db \
+    'INSERT INTO api_clients (created_at, updated_at, created_by_id, client_id, client_secret, scope) VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, "e2e-api", "e2e-api", "$2y$10$.Qp5rC5CVatOexCAW5atju5KILAM.ZK/ZYgvBF6yhmGa6QZVpMAZi", "guild:e2e-api");'
+  E2E_API_HEADER="Basic ZTJlLWFwaTplMmUtYXBpLXNlY3JldA==" make e2e_api
+  exit_code=$?
+fi
 kill $!
 rm main
 exit $exit_code
